5b2847b2ecfdec9b42453412a81a256cbe97f4de
key <key9559@naver.com>
2022-10-21 12:29:12 +0900

    first commit

---
+++ public/assets/js/custom/ui.js
@@ -0,0 +1,30 @@
+// popup
+const $openBtn = document.querySelectorAll(".btn-modal-open");
+
+function popupOpen(name) {
+  var $popupName = document.getElementById(name);
+
+  $popupName.classList.add("active");
+}
+
+function popupClose() {
+  const $popup = document.querySelector(".popup.active");
+
+  $popup.classList.remove("active");
+}
+
+// gnb
+const $gnbActive = document.querySelector(".nav-link.active");
+const $gnbTtl = document.querySelectorAll(".nav-ttl");
+
+for (i = 0; i <= $gnbTtl.length; i++) {
+  let $targetGnbTtl = $gnbTtl[i];
+
+  $targetGnbTtl.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+
+  $gnbActive
+    .closest(".nav-list-wrap")
+    .previousElementSibling.classList.add("active", "on");
+}
