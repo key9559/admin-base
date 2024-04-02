0483904d3bad9b3a0c25ecc98d3f8bb382671f17
고은영(파트너사 (주)라드씨엔에스) <ko.eunyoung01@eland.co.kr>
2024-04-02 09:11:35 +0900

    responsive

--- public/assets/js/custom/ui.js
+++ public/assets/js/custom/ui.js
@@ -1,3 +1,23 @@
+// responsive
+function leftHeaderFold () {
+  if(window.innerWidth <= 768) {
+    document.querySelector('#page').classList.add('tab-size');
+  } else {
+    document.querySelector('#page').classList.remove('tab-size');
+    document.querySelector('.manage-left-hd').classList.remove('tab-active');
+  }
+}
+
+leftHeaderFold();
+
+window.addEventListener('resize', function () {
+  leftHeaderFold()
+});
+
+document.querySelector('.btn-gnb-icon').addEventListener('click', function () {
+  document.querySelector('.manage-left-hd').classList.toggle('tab-active');
+})
+
 // table checked
 let $inputCheckboxs = document.querySelectorAll(".cell-chck [type='checkbox']");
 
@@ -9,11 +29,10 @@ for (i = 0; i <= $inputCheckboxs.length; i++) {
       let $inputCheckeds = document.querySelectorAll(".cell-chck .inp-chck:checked");
       for (i = 0; i <= $inputCheckeds.length; i++) {
         let $inputChecked = $inputCheckeds[i];
-        $inputChecked.closest('.ad-table-row').style.color = '#3B5A93';
+        $inputChecked.closest('.manage-table-row').style.color = '#3B5A93';
       }
     })
   }
-
 }
 
 // gnb
